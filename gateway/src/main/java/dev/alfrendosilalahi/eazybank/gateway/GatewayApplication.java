278e301dd4e058f2d48e80139efd802f401f7ff2
Alfrendo Silalahi <alfrendos72@gmail.com>
2024-12-10 20:28:34 +0700

    feat: Create custom routing using Spring Cloud Gateway

--- gateway/src/main/java/dev/alfrendosilalahi/eazybank/gateway/GatewayApplication.java
+++ gateway/src/main/java/dev/alfrendosilalahi/eazybank/gateway/GatewayApplication.java
@@ -2,12 +2,33 @@ package dev.alfrendosilalahi.eazybank.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator eazybankRoutesConfig(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/eazybank/account/**")
+                        .filters(f -> f.rewritePath("/eazybank/account/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://ACCOUNT"))
+                .route(p -> p
+                        .path("/eazybank/card/**")
+                        .filters(f -> f.rewritePath("/eazybank/card/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://CARD"))
+                .route(p -> p
+                        .path("/eazybank/loan/**")
+                        .filters(f -> f.rewritePath("/eazybank/loan/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://LOAN"))
+                .build();
     }
 
 }
